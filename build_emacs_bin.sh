c52ad2a71d33a6ed276e0ae22dea8e2ab7d63fa6
Hung Dang <hungptit@gmail.com>
2017-02-04 23:54:57 -0500

    Update gtest and leveldb build.

--- build_emacs_bin.sh
+++ build_emacs_bin.sh
@@ -2,58 +2,14 @@
 #  Build emacs related packages  *
 # ********************************
 #!/bin/bash
-
-# Setup build environment
-EXTERNAL_FOLDER=$PWD
-SRC_FOLDER=$EXTERNAL_FOLDER/src
-TMP_FOLDER=/tmp/build/
-
-NCPUS=$(grep -c ^processor /proc/cpuinfo)
-BUILD_OPTS=-j$((NCPUS+1))
-
-# Setup clang
-CLANG=$EXTERNAL_FOLDER/llvm/bin/clang
-CLANGPP=$EXTERNAL_FOLDER/llvm/bin/clang++
-if [ ! -f $CLANGPP ]; then
-    # Fall back to gcc if we do not have clang installed.
-    CLANG=gcc
-    CLANGPP=g++
-fi
-
-# Setup CMake
-CMAKE_PREFIX=$EXTERNAL_FOLDER/cmake
-CMAKE=$CMAKE_PREFIX/bin/cmake
-if [ ! -f $CMAKE ]; then
-    # Use system CMake if we could not find the customized CMake.
-    CMAKE=cmake
-fi
-CMAKE_RELEASE_BUILD="-DCMAKE_BUILD_TYPE:STRING=Release"
-CMAKE_USE_CLANG="-DCMAKE_CXX_COMPILER=${CLANGPP} -DCMAKE_C_COMPILER=${CLANG}"
-
-# Setup git
-GIT_PREFIX=$EXTERNAL_FOLDER/git
-GIT=$GIT_PREFIX/bin/git
-if [ ! -f $GIT ]; then
-    # Use system CMake if we could not find the customized CMake.
-    GIT=git
-fi
-
-mkdir -p $SRC_FOLDER
-mkdir -p $TMP_FOLDER
-
-EMACS_PREFIX=$EXTERNAL_FOLDER/emacs
-mkdir -p $EMACS_PREFIX
+source get_build_options.sh 
 
 # Build silver searcher
 SILVER_SEARCH_GIT=https://github.com/ggreer/the_silver_searcher.git
-SILVER_SEARCH_FOLDER=$SRC_FOLDER/the_silver_searcher
-SILVER_SEARCH_PREFIX=$EMACS_PREFIX/the_silver_searcher
+SILVER_SEARCH_FOLDER=$SRC_DIR/the_silver_searcher
+SILVER_SEARCH_PREFIX=$PREFIX
 
 # Prepare the source code
-cd $SRC_FOLDER
-if [ ! -d $SILVER_SEARCH_FOLDER ]; then
-    git clone $SILVER_SEARCH_GIT
-fi
 cd $SILVER_SEARCH_FOLDER
 git pull
 
@@ -63,39 +19,12 @@ sh build.sh
 make $BUILD_OPTS
 make install
 
-# Build cask
-CASK_FOLDER=$EMACS_PREFIX/cask
-CASK_GIT=https://github.com/cask/cask.git
-cd $EMACS_PREFIX
-
-if [ ! -d $CASK_FOLDER ]; then
-    git clone $CASK_GIT cask
-fi
-cd $CASK_FOLDER
-git pull
-
 # ctags
-CTAGS_LINK=https://github.com/universal-ctags/ctags.git
-CTAGS_SRC=$SRC_FOLDER/ctags
-CTAGS_PREFIX=$EXTERNAL_FOLDER/ctags
-CTAGS_BUILD=$TMP_FOLDER/ctags
-cd $SRC_FOLDER
-if [ ! -d $CTAGS_SRC ]; then
-    $GIT clone $CTAGS_LINK
-fi
-cd $CTAGS_SRC
-git checkout master
-git pull
-./autogen.sh
-./configure --prefix=$CTAGS_PREFIX EXTRA_CFLAGS="-O4 -Wall" EXTRA_CPPFLAGS="-O4"
-make $BUILD_OPTS
-make install
+sh build_using_autogen.sh ctags ""
 
 # Build global
 GLOBAL_LINK=http://tamacom.com/global/
 GLOBAL_FILE=global-6.5.2
-GLOBAL_SRC=$SRC_FOLDER/global
-GLOBAL_PREFIX=$EXTERNAL_FOLDER/global
 
 cd $SRC_FOLDER
 if [ ! -f $GLOBAL_FILE.tar.gz ]; then
@@ -106,23 +35,6 @@ fi
 tar xf $GLOBAL_FILE.tar.gz
 cd $GLOBAL_FILE
 make configure
-./configure --prefix=$GLOBAL_PREFIX CFLAGS="-O4 -Wall" --with-sqlite3 --with-exuberant-ctags=ctags
+./configure --prefix=$PREFIX"-O3 -Wall" --with-sqlite3 --with-exuberant-ctags=ctags
 make $BUILD_OPTS
 make install
-
-# Build Emacs
-EMACS_SRC=$SRC_FOLDER/emacs
-EMACS_LINK=git://git.savannah.gnu.org/emacs.git
-EMACS_PREFIX=$EXTERNAL_FOLDER/emacs
-cd $SRC_FOLDER
-if [ ! -d $EMACS_SRC ]; then
-    git clone $EMACS_LINK
-fi
-
-# Now enter emacs source folder and build
-cd $EMACS_SRC
-git checkout master
-git pull
-./configure --prefix=$EMACS_PREFIX --with-x CFLAGS="-O4"
-make $BUILD_OPTS
-make install $BUILD_OPTS
--- build_google.sh
+++ build_google.sh
@@ -1,56 +1,16 @@
 #!/bin/bash
-
-setup() {
-    EXTERNAL_FOLDER=$PWD
-    SRC_FOLDER=$EXTERNAL_FOLDER/src
-    TMP_FOLDER=/tmp/build/
-
-    mkdir -p $TMP_FOLDER
-    mkdir -p $SRC_FOLDER
-
-    NCPUS=$(grep -c ^processor /proc/cpuinfo)
-    BUILD_OPTS=-j$((NCPUS+1))
-
-    # Setup CMake
-    CMAKE_PREFIX=$EXTERNAL_FOLDER/cmake
-    CMAKE=$CMAKE_PREFIX/bin/cmake
-    if [ ! -f $CMAKE ]; then
-        # Use system CMake if we could not find the customized CMake.
-        CMAKE=cmake
-    fi
-
-    # Setup git
-    GIT_PREFIX=$EXTERNAL_FOLDER/git
-    GIT=$GIT_PREFIX/bin/git
-    if [ ! -f $GIT ]; then
-        # Use system CMake if we could not find the customized CMake.
-        GIT=git
-    fi
-}
-
+source get_build_options.sh 
 
 build_leveldb() {
-    LEVELDB_GIT=https://github.com/google/leveldb
-    LEVELDB_PREFIX=$EXTERNAL_FOLDER/leveldb
-
-    if [ ! -d $LEVELDB_PREFIX ]; then
-        cd $EXTERNAL_FOLDER
-        $GIT clone $LEVELDB_GIT
-    fi
-
-    cd $LEVELDB_PREFIX
+    LEVELDB_DIR=$SRC_DIR/leveldb
+    cd $LEVELDB_DIR
     make clean
     git pull
     make $BUILD_OPTS
-    cd $EXTERNAL_FOLDER
+    cd $ROOT_DIR
 }
 
-# Setup build environment
-setup 
-
 # Build all required packages
-cd $EXTERNAL_FOLDER
-sh build_using_autogen.sh snappy https://github.com/google/snappy.git "" "" "" > /dev/null
-build_leveldb
-sh build_using_cmake.sh $EXTERNAL_FOLDER gtest https://github.com/google/googletest.git
-sh build_using_cmake.sh $EXTERNAL_FOLDER benchmark https://github.com/google/benchmark.git  > /dev/null
+build_leveldb > /dev/null
+sh build_using_cmake.sh gtest > /dev/null 
+sh build_using_cmake.sh benchmark > /dev/null
