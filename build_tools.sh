0b2b59fe466b5999553a7839b27e07e6cf2b472b
Hung Dang <hdang@athenahealth.com>
2017-02-17 12:52:56 -0500

    Fix an automated build for git.

--- build_tools.sh
+++ build_tools.sh
@@ -4,14 +4,23 @@ source ./get_build_options.sh
 # Download all required packages
 cd $ROOT_DIR
 printf "Download all required package for build process!\n"
-sh download_source_code.sh http://ftp.gnu.org/gnu/help2man/help2man-1.47.4.tar.xz help2man
-sh download_source_code.sh http://ftp.gnu.org/gnu/texinfo/texinfo-6.3.tar.xz texinfo
-sh download_source_code.sh http://tukaani.org/xz/xz-5.2.3.tar.gz xz
-sh download_source_code.sh http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz autoconf
-sh download_source_code.sh http://ftp.gnu.org/gnu/automake/automake-1.15.tar.xz automake
-sh download_source_code.sh http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz libtool
-sh download_source_code.sh http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.xz gettext
-sh download_source_code.sh http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz bzip2
+# sh download_source_code.sh http://ftp.gnu.org/gnu/help2man/help2man-1.47.4.tar.xz help2man
+# sh download_source_code.sh http://ftp.gnu.org/gnu/texinfo/texinfo-6.3.tar.xz texinfo
+# sh download_source_code.sh http://tukaani.org/xz/xz-5.2.3.tar.gz xz
+# sh download_source_code.sh http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz autoconf
+# sh download_source_code.sh http://ftp.gnu.org/gnu/automake/automake-1.15.tar.xz automake
+# sh download_source_code.sh http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz libtool
+# sh download_source_code.sh http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.xz gettext
+# sh download_source_code.sh http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz bzip2
+
+# Build git first
+cd $SRC_DIR/git
+git clean -df
+git pull
+make clean
+make configure
+./configure --prefix=$LIBRARIES
+make install $BUILD_OPTS
 
 printf "Build help2man\n"
 sh build_using_configure.sh help2man
@@ -23,10 +32,10 @@ printf "Build xz\n"
 sh build_using_autogen.sh xz > /dev/null
 
 # printf "Build autoconf\n"
-# sh build_using_autogen.sh autoconf > /dev/null
+sh build_using_autogen.sh autoconf > /dev/null
 
 # printf "Build automake\n"
-# sh build_using_autogen.sh automake > /dev/null
+sh build_using_autogen.sh automake > /dev/null
 
 printf "Build libtool\n"
 sh build_using_configure.sh libtool > /dev/null
@@ -66,6 +75,3 @@ sh build_using_autogen.sh lz4 "" "$USE_CLANG" > /dev/null
 
 printf "Build jemalloc\n"
 sh build_using_autogen.sh jemalloc "" "" > /dev/null
-
-# It might take a long time to build git
-# sh build_using_make.sh  git  profile" "PROFILE=BUILD install" > /dev/null
